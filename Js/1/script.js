e46dffed968bf4e1c1649c411ce06c4634737e76
razvan2669 <148984209+razvan2669@users.noreply.github.com>
2026-03-16 10:16:39 +0300

    js1

---
+++ Js/1/script.js
@@ -0,0 +1,25 @@
+
+alert("Привет, мир!");
+console.log("Привет, мир!");
+
+let a = prompt("Введите длину стороны a прямоугольника:", "5");
+let b = prompt("Введите длину стороны b прямоугольника:", "10");
+let sideA = Number(a);
+let sideB = Number(b);
+let S = sideA * sideB;
+console.log("Площадь прямоугольника S = a * b =", S);
+
+let ageInput = prompt("Введите ваш возраст:", "18");
+let age = Number(ageInput);
+
+if (age < 18) {
+  alert("Доступ запрещён. Контент доступен только пользователям 18+.");
+} else {
+  alert("Доступ разрешён. Добро пожаловать на сайт!");
+}
+
+console.log("Задача 4. Таблица умножения числа 5:");
+for (let i = 1; i <= 10; i++) {
+  console.log("5 x " + i + " = " + 5 * i);
+}
+
